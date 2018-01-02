c3a6be78c037d020e3aa9f9b7b8aa309619aca11
Andrzej Thingstad <andrzey.thingstad@gmail.com>
2018-01-02 16:02:08 +0100

    wrong way to merge two arrays

--- src/happening/happening.reducer.ts
+++ src/happening/happening.reducer.ts
@@ -3,7 +3,7 @@ import { ActionKeys, ActionTypes } from './happening.definitions';
 export default function happeningReducer(state = [], action: ActionTypes) {
     switch (action.type) {
         case ActionKeys.HAPPENINGS_FETCH_SUCCEEDED:
-            return { ...state, happenings: action.happenings };
+            return [...state, ...action.happenings];
         default:
             return state;
     }
