fae19a263c1d55764d9ecb660ba328dffb5db295
Andrzej Thingstad <andrzey.thingstad@gmail.com>
2018-01-02 12:27:34 +0100

    removed console.log

--- src/user/user.reducer.ts
+++ src/user/user.reducer.ts
@@ -3,7 +3,6 @@ import { ActionKeys, ActionTypes } from './user.definitions';
 export default function userReducer(state = {}, action: ActionTypes) {
     switch (action.type) {
         case ActionKeys.USER_FETCH_SUCCEEDED:
-            console.log(action);
             return { ...state, user: action.user };
         default:
             return state;
